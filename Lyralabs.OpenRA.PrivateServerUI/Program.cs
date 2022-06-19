aacd4ab82be7a4c9e86fd93f2e10deb4f0277f6b
Lyra <git@lyra.bz>
2022-06-19 19:33:25 +0200

    refactor

--- Lyralabs.OpenRA.PrivateServerUI/Program.cs
+++ Lyralabs.OpenRA.PrivateServerUI/Program.cs
@@ -2,7 +2,7 @@ namespace Company.WebApplication1
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -16,24 +16,19 @@ namespace Company.WebApplication1
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment() == false)
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
-
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.MapBlazorHub();
             app.MapFallbackToPage("/_Host");
 
-            app.Run();
+            await app.RunAsync();
         }
     }
 }
