d60ce6b02cfd15ba6c21f2f0712903f7d0de55d7
Lyra <git@lyra.bz>
2022-06-19 21:40:06 +0200

    added ui

---
+++ Lyralabs.OpenRA.PrivateServerUI/Pages/GameServerUserConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Lyralabs.OpenRA.PrivateServerUI.Services;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lyralabs.OpenRA.PrivateServerUI.Pages
+{
+    public class GameServerUserConfiguration
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public OpenRAMod Mod { get; set; } = OpenRAMod.ra;
+
+        public bool AdvertiseOnline { get; set; } = true;
+
+        public bool EnableSingleplayer { get; set; } = true;
+    }
+}
--- Lyralabs.OpenRA.PrivateServerUI/Pages/Index.razor.cs
+++ Lyralabs.OpenRA.PrivateServerUI/Pages/Index.razor.cs
@@ -1,7 +1,17 @@
-﻿namespace Lyralabs.OpenRA.PrivateServerUI.Pages
+﻿using Lyralabs.OpenRA.PrivateServerUI.Services;
+
+namespace Lyralabs.OpenRA.PrivateServerUI.Pages
 {
     public class IndexViewModel : ComponentBase
     {
+        public GameServerUserConfiguration Model { get; set; } = null!;
 
+        [Inject]
+        public GameServerService GameServerService { get; set; } = null!;
+
+        public void CreateServer()
+        {
+            
+        }
     }
 }
